6ac3d238d764bcfb99a241c32745d93a0a143782
Thomas Heller <info@zilence.net>
2014-06-11 15:31:12 +0200

    update deps, bump version

--- project.clj
+++ project.clj
@@ -1,14 +1,15 @@
-(defproject thheller/shadow-build "0.5.0"
+(defproject thheller/shadow-build "0.6.0"
   :description "cljs compiler"
   :url "https://github.com/thheller/shadow-build"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [aysylu/loom "0.4.1"]]
+  :dependencies []
 
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                   :dependencies [[org.clojure/clojure "1.6.0"]
+                                  [aysylu/loom "0.5.0"]
+                                  [org.clojure/tools.namespace "0.2.4"]
                                   [org.clojure/clojurescript "0.0-2138"
                                    :exclusions [org.clojure/google-closure-library]]]}} 
   
