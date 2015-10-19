b7f58141829df53416b39e568554b524f463e840
Thomas Heller <info@zilence.net>
2015-10-19 11:35:35 +0200

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-build "1.0.127"
+(defproject thheller/shadow-build "1.0.131"
   :description "cljs compiler"
   :url "https://github.com/thheller/shadow-build"
   :license {:name "Eclipse Public License"
