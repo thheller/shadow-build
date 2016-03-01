00e81b04946563f60920f252ad9026a28360098d
Thomas Heller <info@zilence.net>
2016-03-01 10:21:46 +0100

    bump clojure

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.228"]
                  [aysylu/loom "0.5.0"]
                  [com.cognitect/transit-clj "0.8.283"]]
