d0027c49cc0a85eab463fc1de48a02e72b0235fd
Thomas Heller <th.heller@gmail.com>
2017-04-06 21:31:48 +0200

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-build "1.0.20170405"
+(defproject thheller/shadow-build "1.0.20170406"
   :description "cljs compiler"
   :url "https://github.com/thheller/shadow-build"
   :license
