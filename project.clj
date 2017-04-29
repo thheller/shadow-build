2eb731ac4b3169632b157d9564584e2ee92aafdd
Thomas Heller <th.heller@gmail.com>
2017-04-29 09:57:54 +0200

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-build "1.0.20170413"
+(defproject thheller/shadow-build "1.0.20170429"
   :description "cljs compiler"
   :url "https://github.com/thheller/shadow-build"
   :license
@@ -7,7 +7,7 @@
 
   :dependencies
   [[org.clojure/clojure "1.9.0-alpha15"]
-   [org.clojure/clojurescript "1.9.518"]
+   [org.clojure/clojurescript "1.9.521"]
    [com.cognitect/transit-clj "0.8.300"
     :exclusions
     [org.msgpack/msgpack]]
