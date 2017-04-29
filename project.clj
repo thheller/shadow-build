1d58bb17d04255abd2ed99897156ded521e459f5
Thomas Heller <th.heller@gmail.com>
2017-04-29 17:02:18 +0200

    need some extra methods on goog.global in node

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-build "1.0.20170429"
+(defproject thheller/shadow-build "1.0.20170429-17"
   :description "cljs compiler"
   :url "https://github.com/thheller/shadow-build"
   :license
