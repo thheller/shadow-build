8ede075734752e1488408622e9f35516b2340372
Thomas Heller <th.heller@gmail.com>
2017-05-12 13:28:42 +0200

    release bugfix

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject thheller/shadow-build "1.0.20170512"
+(defproject thheller/shadow-build "1.0.20170512-13"
   :description "cljs compiler"
   :url "https://github.com/thheller/shadow-build"
   :license
